4d55eb61ab13baab9beb9465b57e067ec7e97e6a
Sangjune park <74230343+tkdwns414@users.noreply.github.com>
2024-04-12 02:26:55 +0900

    [Silver IV] Title: 제로, Time: 232 ms, Memory: 23724 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Stack;
+import java.util.ArrayDeque;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
@@ -11,7 +9,7 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 		
 		int num = Integer.parseInt(br.readLine());
-		Stack<Integer> stk = new Stack<>();
+		ArrayDeque<Integer> stk = new ArrayDeque<>();
 		
 		for (int i = 0; i < num; i++) {
 			int n = Integer.parseInt(br.readLine());
