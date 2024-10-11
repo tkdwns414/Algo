a24948e7eca01ef4c63293c2751cfdc027866f4b
Sangjune park <74230343+tkdwns414@users.noreply.github.com>
2024-10-11 18:28:11 +0900

    [level 3] Title: 네트워크, Time: 1.81 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,24 +1,23 @@
 def solution(n, computers):
     visited = [0] * n
-    network_num = 1
+    network = 1
 
-    def dfs(net, visited, node):
-        stack = [node]
+    def dfs(node, network):
+        stack = []
+        stack.append(node)
+        
         while stack:
-            cur_node = stack.pop()
+            cur = stack.pop()
+            visited[cur] = network
             for i in range(n):
-                if computers[cur_node][i] == 1 and visited[i] == 0:
-                    visited[i] = net
+                if computers[cur][i] and not visited[i]:
                     stack.append(i)
-                    
+                
+        
     for i in range(n):
-        if visited[i] == 0: # 방문하지 않은 곳(네트워크가 형성되지 않은 곳)
-            dfs(network_num, visited, i)
-            network_num += 1
+        if not visited[i]:
+            dfs(i, network)
+            network += 1
             
-    print(visited)
     
-    return max(visited)
-
-
-# 0부터 n-1까지 돌아가면서 visited가 아닐 때 dfs를 실행
+    return network - 1
