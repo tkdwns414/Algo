3859bbc88e5e7b44e5714c194fbda462642abc0a
Sangjune park <74230343+tkdwns414@users.noreply.github.com>
2024-10-11 16:23:22 +0900

    [level 2] Title: H-Index, Time: 604.31 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+def solution(citations):
+    
+    max_h = 0
+    counter = Counter(citations)
+    
+    for h in range(1, 10001):
+        check = [citation >= h for citation in citations]
+        if check.count(True) >= h and check.count(False) <= h:
+            max_h = max(max_h, h)
+        
+    
+    return max_h
+
+# n편 중 h 번 이상 인용된 논문이 h편 이상이고 나머지 논문이 h번 이하 인용되었다
