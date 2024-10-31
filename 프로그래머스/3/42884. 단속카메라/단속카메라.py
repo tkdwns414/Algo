19d0455dc565a2d9bb46061c8c5de23be6f2bb4c
Sangjune park <74230343+tkdwns414@users.noreply.github.com>
2024-10-31 18:07:56 +0900

    [level 3] Title: 단속카메라, Time: 3.27 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,19 +1,3 @@
 def solution(routes):
-    routes.sort()
-    current = 0
-    temp = routes[0]
-    answer = 1 # 제일 기본 범위 하나 가지고 시작
-    
-    for idx, route in enumerate(routes):
-        # 겹치는 부분이 있다면 겹치는 부분으로 temp를 업데이트
-        # 겹치는 부분이 없다면 answer += 1 temp를 새 범위로
-        if temp[0] <= route[0] <= temp[1]: # 정렬해서 앞에만 비교하면 됨
-            temp[0] = max(temp[0], route[0])
-            temp[1] = min(temp[1], route[1])
-        else:
-            temp = route
-            answer += 1
-        
+    answer = 0
     return answer
-
-# 왜 굳이 지점에 음수가 존재하지??
